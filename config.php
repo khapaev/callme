714c71fa9f38a0f51db7fd1b391fc1a44a4488f1
Alexander Khapaev <alexander@khapaev.ru>
2023-06-01 17:31:57 +0300

    Update configuration file

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = [
-	'CallMeDEBUG' => 1, // Дебаг сообщения в логе: 1 - пишем, 0 - не пишем
+	'CallMeDEBUG' => true, // Дебаг сообщения в логе: true - пишем, false - не пишем
 	'bitrixApiUrl' => 'https://_type_your_url_here', // URL к API Битрикс24 (входящий вебхук)
-	'asterisk' => array( // Настройки для подключения к Asterisk
+	'asterisk' => [ // Настройки для подключения к Asterisk
 		'host' => '127.0.0.1',
 		'scheme' => 'tcp://',
 		'port' => 5038,
@@ -11,7 +11,7 @@ $config = [
 		'secret' => 'MVGuslitSTBT',
 		'connect_timeout' => 10000,
 		'read_timeout' => 10000
-	),
+	],
 	'listener_timeout' => 300, // Скорость обработки событий от Asterisk
 ];
 
